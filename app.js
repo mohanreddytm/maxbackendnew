d4774c764062024a90817e3fd502adffa4ab5295
mohanreddytm <tamminenimohanreddy1@gmail.com>
2025-05-18 15:54:14 +0530

    Max backend initial commmit

--- app.js
+++ app.js
@@ -5,13 +5,58 @@ const fetch = require('node-fetch');
 const app = express();
 const PORT = 5050;
 
+const bcrypt = require('bcrypt');
+
+
 app.use(cors());
 app.use(express.json());
 
 require("dotenv").config();
 
+const {Pool} = require('pg');
+
+const databaseUrl = process.env.DATABASE_URL ;
+
+const pool = new Pool({
+  connectionString: databaseUrl ,
+});
+
 const clientId = process.env.EBAY_CLIENT_ID;
 const clientSecret = process.env.EBAY_CLIENT_SECRET;
+
+app.get('/getUsers', async (req, res) => {
+  try{
+    const query = 'SELECT * FROM public."Users";';
+    const response = await pool.query(query);
+    if (response.rows.length === 0) {
+      return res.status(404).json({ error: 'No users found' });
+    }
+    res.status(200).json(response.rows);
+  }catch(err){
+    console.error('Error fetching users:', err);
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+app.post('/addUser', async (req, res) => {
+  const { createdAt, updatedAt, firstname, lastname, password, profileImage, role, isActive, email, emailVerified } = req.body;
+  const name = `${firstname} ${lastname}`;
+  const username = `${firstname}_${lastname}`;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const query = 'INSERT INTO "Users" (created_at, last_login,username, full_name, password_hash, avatar_url, role, is_active, email, email_verified) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *';
+  const values = [createdAt, updatedAt, username, name, hashedPassword, profileImage, role, isActive, email, emailVerified];
+  try {
+    const response = await pool.query(query, values);
+    if (response.rows.length > 0) {
+      res.status(201).json(response.rows[0]);
+    } else {
+      res.status(400).json({ error: 'Failed to add user' });
+    }
+  } catch (err) {
+    console.error('Error adding user:', err);
+    res.status(500).json({ error: 'Failed to add user' });
+  }
+});
 
 app.get('/get-ebay-token', async (req, res) => {
   const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
@@ -55,6 +100,8 @@ app.get('/search-ebay-products', async (req, res) => {
     res.status(500).json({ error: 'Error fetching products from eBay' });
   }
 });
+
+
 
 
 app.listen(PORT, () => {
